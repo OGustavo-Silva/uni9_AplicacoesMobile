7e8785fb0de887fd36006d3de1eb10e153ccd6e2
Gustavo Silva <gustavos_barros@hotmail.com>
2021-09-24 22:09:02 -0300

    consumo de API

---
+++ exercicio/lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  TextEditingController input1Controller = TextEditingController();
+
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Exercicio 24/9"),
+      ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                controller: input1Controller,
+                decoration: InputDecoration(
+                  labelText: "Entrada 1",
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
